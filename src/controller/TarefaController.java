13a325a175323b6aa5de4f14777e3c01cc72dae4
Rafeixons <goncalvesrafeixons@gmail.com>
2026-05-28 20:10:11 -0300

    Finalização do código. Adição de funçoes e métodos

--- src/controller/TarefaController.java
+++ src/controller/TarefaController.java
@@ -4,51 +4,42 @@ import model.TarefaModel;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
-
 public class TarefaController {
-    
+
     TarefaModel tarefa = new TarefaModel();
-    
+
     // Lista de tarefas
-    
     private ArrayList<TarefaModel> lista = new ArrayList<>();
-    
+
     //Metodo Adicionar
-    
-    public void adicionar(String nome){   
+    public void adicionar(String nome) {
         TarefaModel t = new TarefaModel();
         t.setNome(nome);
         lista.add(t);
     }
-    
+
     //Metodo Listar
-    
-    public ArrayList<TarefaModel> listar (){
+    public ArrayList<TarefaModel> listar() {
         return lista;
     }
-    
-    public int tamanho (){
+
+    public int tamanho() {
         return lista.size();
     }
-    
-    public void concluir (boolean a){
-        if(!lista.isEmpty()){
+
+    public void concluir(boolean a) {
+        if (!lista.isEmpty()) {
             lista.get(lista.size() - 1).setConcluida(a);
         }
     }
-    
-    public void excluir (String nome){
-        for (TarefaModel t : lista){
-            if (t.getNome().equalsIgnoreCase(nome)){
+
+    public void excluir(String nome) {
+        for (TarefaModel t : lista) {
+            if (t.getNome().equalsIgnoreCase(nome)) {
                 lista.remove(t);
             }
-            
+
         }
-            
-        }
+
     }
-        
-        
-    
-
-
+}
--- src/view/TarefaView.java
+++ src/view/TarefaView.java
@@ -1,4 +1,3 @@
-
 package view;
 
 import model.TarefaModel;
@@ -7,14 +6,14 @@ import javax.swing.JOptionPane;
 import java.util.ArrayList;
 
 public class TarefaView extends javax.swing.JFrame {
-    
+
     TarefaController controller = new TarefaController();
     ArrayList<TarefaModel> lista = new ArrayList<>();
-    
 
     public TarefaView() {
         initComponents();
     }
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -28,15 +27,10 @@ public class TarefaView extends javax.swing.JFrame {
         rb_não = new javax.swing.JRadioButton();
         jLabel2 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        ta_lista = new javax.swing.JTextArea();
-        jb_listar = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
         txf_quant = new javax.swing.JTextField();
-        jPanel3 = new javax.swing.JPanel();
-        txf_excluir = new javax.swing.JTextField();
-        jb_excluir = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
+        cb_lista = new javax.swing.JComboBox<>();
+        jb_ok = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("To Do List");
@@ -123,17 +117,6 @@ public class TarefaView extends javax.swing.JFrame {
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Lista de Tarefas"));
         jPanel2.setToolTipText("Cadastro de Tarefas");
 
-        ta_lista.setColumns(20);
-        ta_lista.setRows(5);
-        jScrollPane1.setViewportView(ta_lista);
-
-        jb_listar.setText("Listar Tarefas");
-        jb_listar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jb_listarActionPerformed(evt);
-            }
-        });
-
         jLabel3.setText("Quantidade de tarefas cadastradas: ");
 
         txf_quant.setEditable(false);
@@ -144,76 +127,47 @@ public class TarefaView extends javax.swing.JFrame {
             }
         });
 
+        jb_ok.setText("OK");
+        jb_ok.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jb_okActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1)
-                .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jb_listar)
-                .addGap(158, 158, 158))
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(23, 23, 23)
-                .addComponent(jLabel3)
-                .addGap(18, 18, 18)
-                .addComponent(txf_quant, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(41, 41, 41)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(cb_lista, javax.swing.GroupLayout.PREFERRED_SIZE, 321, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addGap(18, 18, 18)
+                                .addComponent(txf_quant, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(159, 159, 159)
+                        .addComponent(jb_ok)))
+                .addContainerGap(56, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(21, 21, 21)
+                .addComponent(cb_lista, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jb_listar)
-                .addGap(4, 4, 4)
+                .addComponent(jb_ok)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(txf_quant, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(txf_quant, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(11, 11, 11))
         );
 
-        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 300, 428, -1));
-
-        jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder("Excluir Tarefa"));
-
-        jb_excluir.setText("Excluir");
-        jb_excluir.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jb_excluirActionPerformed(evt);
-            }
-        });
-
-        jLabel4.setText("Nome da Tarefa: ");
-
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel4)
-                .addGap(38, 38, 38)
-                .addComponent(txf_excluir, javax.swing.GroupLayout.PREFERRED_SIZE, 179, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jb_excluir)
-                .addContainerGap(12, Short.MAX_VALUE))
-        );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
-                .addContainerGap(31, Short.MAX_VALUE)
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jb_excluir)
-                    .addComponent(jLabel4)
-                    .addComponent(txf_excluir, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(29, 29, 29))
-        );
-
-        getContentPane().add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 180, 430, 110));
+        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 180, 428, 190));
 
         pack();
         setLocationRelativeTo(null);
@@ -224,39 +178,44 @@ public class TarefaView extends javax.swing.JFrame {
     }//GEN-LAST:event_txf_nomeActionPerformed
 
     private void jb_adicionarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jb_adicionarActionPerformed
-       String nome = txf_nome.getText();
-       controller.adicionar(nome);
-       String mensagem  = "";
-       mensagem += "Tarefa Cadastrada";
-       
-       
-       if (rb_sim.isSelected()) {
+        String nome = txf_nome.getText();
+
+        if (txf_nome.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Digite um nome para sua tarefa!");
+            return;
+        }
+
+        for (int i = 0; i < cb_lista.getItemCount(); i++) {
+            String vericar = cb_lista.getItemAt(i).toString();
+
+            if (nome.equalsIgnoreCase(vericar)) {
+                JOptionPane.showMessageDialog(null, "Tarefa com o mesmo nome já cadastrada!");
+                return;
+            }
+        }
+
+        cb_lista.addItem(txf_nome.getText());
+
+        controller.adicionar(nome);
+        String mensagem = "";
+        mensagem += "Tarefa Cadastrada";
+
+        if (rb_sim.isSelected()) {
             controller.concluir(true);
             mensagem += "\nTarefa marcada como concluida!";
         }
-       if (rb_não.isSelected()) {
+        if (rb_não.isSelected()) {
             controller.concluir(false);
             mensagem += "\nTarefa em andamento!";
         }
-       JOptionPane.showMessageDialog(null, mensagem);
-       txf_nome.setText("");
+        JOptionPane.showMessageDialog(null, mensagem);
+        txf_nome.setText("");
+
+
     }//GEN-LAST:event_jb_adicionarActionPerformed
 
-    private void jb_listarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jb_listarActionPerformed
-          ta_lista.setText("");
-        // Percorre a lista
-        
-        for(TarefaModel t : controller.listar()){
-            ta_lista.append(t.toString());
-            ta_lista.append("\n");            
-        }
-        
-        String tamanho = String.valueOf(controller.tamanho());
-        txf_quant.setText(tamanho);
-    }//GEN-LAST:event_jb_listarActionPerformed
-
     private void rb_simActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rb_simActionPerformed
-        
+
     }//GEN-LAST:event_rb_simActionPerformed
 
     private void rb_nãoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rb_nãoActionPerformed
@@ -264,18 +223,44 @@ public class TarefaView extends javax.swing.JFrame {
     }//GEN-LAST:event_rb_nãoActionPerformed
 
     private void txf_quantActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txf_quantActionPerformed
-       String tamanho = String.valueOf(lista.size());
+        String tamanho = String.valueOf(lista.size());
         txf_quant.setText(tamanho);
     }//GEN-LAST:event_txf_quantActionPerformed
 
-    private void jb_excluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jb_excluirActionPerformed
-       String nome = txf_excluir.getText().trim();
-       controller.excluir(nome);
-    }//GEN-LAST:event_jb_excluirActionPerformed
+    private void jb_okActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jb_okActionPerformed
 
-   
+        String tamanho = String.valueOf(controller.tamanho());
+        txf_quant.setText(tamanho);
+
+        if (cb_lista.toString().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Nenhuma atividade cadastrada!");
+        } else {
+            String[] opcoes = {"Listar", "Excluir"};
+
+            int escolha = JOptionPane.showOptionDialog(
+                    null,
+                    cb_lista.getSelectedItem().toString(),
+                    "Oque fazer com essa tarefa ? ", //Titulo da caixa
+                    JOptionPane.DEFAULT_OPTION, //Botões desabilitados
+                    JOptionPane.QUESTION_MESSAGE, //Icones
+                    null, //Sempre null
+                    opcoes, //Vetor 
+                    opcoes[0]);                         //Inicio do vetor
+
+            if (escolha == 0) {
+                JOptionPane.showMessageDialog(null, controller.listar());
+            } else if (escolha == 1) {
+                String selecionado = cb_lista.getSelectedItem().toString();
+                cb_lista.removeItem(selecionado);
+                JOptionPane.showMessageDialog(null, "Tarefa excluida!");
+            }
+
+
+    }//GEN-LAST:event_jb_okActionPerformed
+    }
+
     public static void main(String args[]) {
-    
+
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new TarefaView().setVisible(true);
@@ -285,21 +270,16 @@ public class TarefaView extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup btg_concluir;
+    private javax.swing.JComboBox<String> cb_lista;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton jb_adicionar;
-    private javax.swing.JButton jb_excluir;
-    private javax.swing.JButton jb_listar;
+    private javax.swing.JButton jb_ok;
     private javax.swing.JRadioButton rb_não;
     private javax.swing.JRadioButton rb_sim;
-    private javax.swing.JTextArea ta_lista;
-    private javax.swing.JTextField txf_excluir;
     private javax.swing.JTextField txf_nome;
     private javax.swing.JTextField txf_quant;
     // End of variables declaration//GEN-END:variables
